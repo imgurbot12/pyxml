e8352378d9354bb6ee7da053e903e32aa6a84811
Elis Byberi <elis.byberi@gmail.com>
2023-04-06 19:55:50 +0200

    Added List of TODO items (non-exhaustive)
    To keep track of incompatibilities, we can include a section in the module that specifically addresses any known incompatibilities.

--- xml/_compat/__init__.py
+++ xml/_compat/__init__.py
@@ -1,21 +1,28 @@
 try:
     from typing import NamedTuple
     from typing import Optional
-    from typing import Iterator
+    from typing import Iterator  # incompatible
     from typing import List
-    from typing import Generator
+    from typing import Generator  # incompatible
     from typing import Dict
-    from typing import Any
-    from typing import BinaryIO
+    from typing import Any  # incompatible
+    from typing import BinaryIO  # incompatible
     from typing import Tuple
-    from typing import Callable
+    from typing import Callable  # incompatible?, undocumented
     from typing import Union
 
-    from functools import wraps
-    from enum import IntEnum
+    from functools import wraps  # missing
+    from enum import IntEnum  # missing
     import string
 
     from typing_extensions import Self
 
 except:
     raise NotImplementedError
+
+# TODO (non-exhaustive):
+#  Iterator may be replace with List
+#  Generator has only 1 usage at BaseLexer.iter()
+#  Any has only 1 usage in Element.get()
+#  Callable is undocumented, do test
+#  replicate class: string, bytes, BinaryIO
