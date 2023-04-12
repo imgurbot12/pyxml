440defa1188b261d56f11be32e06f661d2696323
imgurbot12 <imgurbot12@gmail.com>
2023-04-12 15:47:47 -0700

    feat: include setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+setup(
+    name='pyxml3',
+    version='0.0.1',
+    license='MIT',
+    author='Andrew Scott',
+    author_email='imgurbot12@gmail.com',
+    url='https://github.com/imgurbot12/pypub',
+    description="Pure python3 Alternative to stdlib xml.etree with HTML support",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+    packages=find_packages(),
+    install_requires=[
+        'dataclasses',
+        'typing_extensions',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
